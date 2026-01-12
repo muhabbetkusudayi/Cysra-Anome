8574fadf4867547d7e192399b9c6f219811bb769
muhabbetkusudayi <cicikuslastim@outlook.com>
2026-01-12 21:08:46 +0300

    Changed Name to Cysra Anome Browser Lite

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo off
 echo =========================
-echo   Building SysraBrowser
+echo   Building Cysra Anome
 echo =========================
 
 python -m pip install --upgrade pip
 python -m pip install pyinstaller pyqt5 pyqtwebengine
 
-python -m PyInstaller SysraBrowser.spec --clean --noconfirm
+python -m PyInstaller CysraAnome.spec --clean --noconfirm
 
 echo =========================
 echo Build completed!
